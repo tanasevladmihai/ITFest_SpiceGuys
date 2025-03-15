82b891fc6f75e6fc6dcf1f7f9df0134f04c38731
tanasevladmihai <vmtanase@gmail.com>
2025-03-15 18:24:41 +0200

    Database correction
    Am resuti sa dau set-up calumea la baza de date

--- ITFest_hackathon_WhereTo/where_to/settings.py
+++ ITFest_hackathon_WhereTo/where_to/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-GOOGLE_MAPS_API_KEY = 'your-google-maps-api-key'
+GOOGLE_MAPS_API_KEY = 'REDACTED'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
